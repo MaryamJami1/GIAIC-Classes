9715c6c936a231c9c39d4c28edad55f47d2834c7
MaryamJami1 <maryamchouhan373@gmail.com>
2024-07-18 15:04:02 +0500

    practice

--- all topics/app.ts
+++ all topics/app.ts
@@ -837,13 +837,13 @@
 // )
 
 
-function delaymessage(callback: () => void): void {
-    setTimeout(() => {
-        callback()
-    }, 1000)
-}
+// function delaymessage(callback: () => void): void {
+//     setTimeout(() => {
+//         callback()
+//     }, 1000)
+// }
 
-delaymessage(()=>{
-    console.log("hi i am callback");
+// delaymessage(()=>{
+//     console.log("hi i am callback");
     
-})
+// })
